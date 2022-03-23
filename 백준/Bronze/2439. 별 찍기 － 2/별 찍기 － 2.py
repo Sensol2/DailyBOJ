0c3e3d22b73d4defce32b01f8e502dcba266ebe4
Sensol2 <80695040+Sensol2@users.noreply.github.com>
2022-03-23 14:16:12 +0900

    [Bronze III] Title: 별 찍기 - 2, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -0,0 +1,10 @@
+import sys
+n = int(sys.stdin.readline())
+
+for i in range(1, n+1):
+	for j in range(1, n+1):
+		if (j <= n-i):
+			print(" ", end="")
+		else:
+			print("*", sep="", end="")
+	print("")
