32318f9a664dfa3e0af35e3c9f52821f7c498665
Sensol2 <80695040+Sensol2@users.noreply.github.com>
2022-03-23 15:42:36 +0900

    [Bronze IV] Title: 사분면 고르기, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,11 @@
+x = int(input())
+y = int(input())
+
+if x > 0 and y > 0: # 1
+	print("1")
+if x < 0 and y > 0: # 2
+	print("2")
+if x < 0 and y < 0: # 3
+	print("3")
+if x > 0 and y < 0: # 4
+	print("4")
