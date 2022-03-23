eacf166404039320c0a3219795dc0e9fd3c2750c
Sensol2 <80695040+Sensol2@users.noreply.github.com>
2022-03-23 14:27:24 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 72 ms, Memory: 32976 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.py
@@ -0,0 +1,17 @@
+# 파이썬 문제풀이용 환경세팅
+# [라이브러리]
+import sys, time
+import math
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~파도가 친다 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# [자주 쓰는 입력 모듈]
+# 1) 정수 한 개 입력
+# num = int(sys.stdin.readline())
+# 2) 여러개 입력
+# a, b, c = map(int,sys.stdin.readline().split())
+# 3) 여러개 리스트로 입력
+# arr = list(map(int,sys.stdin.readline().split()))
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~파도가 친다 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+a, b= map(int,sys.stdin.readline().split())
+print(a+b)
