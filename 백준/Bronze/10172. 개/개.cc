988917c8eec03d52a1c7de80c592c96ca5ef8533
Sensol2 <80695040+Sensol2@users.noreply.github.com>
2022-03-23 15:51:58 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	printf("|\\_/|");
+	printf("\n");
+	printf("|q p|   /}");
+	printf("\n");
+	printf("( 0 )\"\"\"\\");
+	printf("\n");
+	printf("|\"^\"\`    |");
+	printf("\n");
+	printf("||_/=\\\\__|");
+}
