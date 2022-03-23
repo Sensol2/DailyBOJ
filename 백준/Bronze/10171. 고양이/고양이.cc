17c0917f67e57c6c21c529100c30b98890da3f9d
Sensol2 <80695040+Sensol2@users.noreply.github.com>
2022-03-23 14:26:22 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	printf("\\    /\\");
+	printf("\n");
+	printf(" )  ( \')");
+	printf("\n");
+	printf("(  /  )");
+	printf("\n");
+	printf(" \\(__)|");
+}
