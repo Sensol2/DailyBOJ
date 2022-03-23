31bda9cdb3fd446dd563b5b7d47efeb8864d4dd9
Sensol2 <80695040+Sensol2@users.noreply.github.com>
2022-03-23 15:50:35 +0900

    [Bronze II] Title: 알파벳 개수, Time: 164 ms, Memory: 37884 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,18 @@
+# 파이썬 문제풀이용 환경세팅
+# [라이브러리]
+from calendar import c
+import sys, time
+import math
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~파도가 친다 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# [자주 쓰는 입력 모듈]
+# 1) 정수 한 개 입력
+# num = int(sys.stdin.readline())
+# 2) 여러개 입력
+# a, b, c = map(int,sys.stdin.readline().split())
+# 3) 여러개 리스트로 입력
+# arr = list(map(int,sys.stdin.readline().split()))
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~파도가 친다 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+s = sys.stdin.readline().rstrip()
+
+for i in range(ord('a'), ord('z')+1):
+    print(s.count(chr(i)), end=' ')
