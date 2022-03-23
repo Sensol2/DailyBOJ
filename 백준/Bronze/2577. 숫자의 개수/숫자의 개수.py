130764728e3e219028eb6384f8e033009d07fff9
Sensol2 <80695040+Sensol2@users.noreply.github.com>
2022-03-23 15:36:02 +0900

    [Bronze II] Title: 숫자의 개수, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -0,0 +1,18 @@
+import sys, time
+
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+c = int(sys.stdin.readline())
+
+num = str(a * b * c)
+
+print(num.count("0"))
+print(num.count("1"))
+print(num.count("2"))
+print(num.count("3"))
+print(num.count("4"))
+print(num.count("5"))
+print(num.count("6"))
+print(num.count("7"))
+print(num.count("8"))
+print(num.count("9"))
