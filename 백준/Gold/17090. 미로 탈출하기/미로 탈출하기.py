e93c8ebed08bb88ddadd117b9b247585b2ab4941
Sensol2 <80695040+Sensol2@users.noreply.github.com>
2022-04-27 13:35:24 +0900

    [Gold III] Title: 미로 탈출하기, Time: 260 ms, Memory: 130108 KB -BaekjoonHub

--- 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.py
+++ 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.py
@@ -3,9 +3,9 @@ from collections import deque
 N, M = map(int, input().split())
 maze = [[] * M for _ in range(N)]
 
-# 상,하,좌,우
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+# 상,좌,하,우
+dx = [0, -1, 0, 1]
+dy = [-1, 0, 1, 0]
 
 for i in range(N):
     maze[i] = list(input())
@@ -15,31 +15,20 @@ queue = deque()
 cnt = 0
 visited = [[False] * M for _ in range(N)] 
 
-# 꼭짓점 제외하고, 탈출할 수 있는 칸 추가
+# 탈출할 수 있는 칸 추가
 for y in range(N):
     for x in range(M):
-        if y == 0 and maze[y][x] == 'U': # 윗변
-            queue.append((y,x))
-            visited[y][x] = True
-            cnt += 1
-        if y == N-1 and maze[y][x] == 'D': # 아랫변
-            queue.append((y,x))
-            visited[y][x] = True
-            cnt += 1
-        if x == 0 and maze[y][x] == 'L': # 왼변
-            queue.append((y,x))
-            visited[y][x] = True
-            cnt += 1
-        if x == M-1 and maze[y][x] == 'R': # 오른변
+        if (y == 0 and maze[y][x] == 'U') or (y == N-1 and maze[y][x] == 'D') or (x == 0 and maze[y][x] == 'L') or (x == M-1 and maze[y][x] == 'R'):
             queue.append((y,x))
             visited[y][x] = True
             cnt += 1
 
+direction_list = ['U', 'L', 'D', 'R']
 while queue:
     item = queue.popleft()
     y, x = item[0], item[1]
 
-    for i in range(4): # 상,하,좌,우
+    for i in range(4): # 상,좌,하,우
         tx = x + dx[i]
         ty = y + dy[i]
 
@@ -47,19 +36,7 @@ while queue:
             continue
         
         if not visited[ty][tx]:
-            if i == 0 and maze[ty][tx] == 'D': # 윗쪽 칸이 D인 경우
-                visited[ty][tx] = True
-                queue.append((ty, tx))
-                cnt += 1
-            if i == 1 and maze[ty][tx] == 'U': # 아랫쪽 칸이 U인 경우
-                visited[ty][tx] = True
-                queue.append((ty, tx))
-                cnt += 1
-            if i == 2 and maze[ty][tx] == 'R': # 왼쪽 칸이 R인 경우
-                visited[ty][tx] = True
-                queue.append((ty, tx))
-                cnt += 1
-            if i == 3 and maze[ty][tx] == 'L': # 오른쪽 칸이 L인 경우
+            if maze[ty][tx] == direction_list[(i+2)%4]:
                 visited[ty][tx] = True
                 queue.append((ty, tx))
                 cnt += 1
