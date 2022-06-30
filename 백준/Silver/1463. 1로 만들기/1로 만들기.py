5f31b0625532febeafe8354b3af145e2b87c5808
Sensol2 <80695040+Sensol2@users.noreply.github.com>
2022-06-30 10:31:49 +0900

    [Silver III] Title: 1로 만들기, Time: 248 ms, Memory: 139268 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,40 +1,24 @@
-# 파이썬 문제풀이용 환경세팅
-# [라이브러리]
-from calendar import c
-import sys, time
-import math
+def Calculator(n, depth):
+    if n == 1:
+        return depth
+    if DP[n]:
+        return DP[n] + depth
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~파도가 친다 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# [자주 쓰는 입력 모듈]
-# 1) 정수 한 개 입력
-# num = int(sys.stdin.readline())
-# 2) 여러개 입력
-# a, b, c = map(int,sys.stdin.readline().split())
-# 3) 여러개 리스트로 입력
-# arr = list(map(int,sys.stdin.readline().split()))
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~파도가 친다 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-count = 0
-arr = dict()
-def dp(n):
-    global count
-    if n in arr.keys():
-        return arr[n]
-    count += 1
     best = []
+    depth += 1
     if n % 3 == 0:
-        best.append(dp(n//3))
+        best.append(Calculator(n//3, depth))
     if n % 2 == 0:
-        best.append(dp(n//2))
-    best.append(dp(n-1))
-    arr[n] = min(best) + 1
+        best.append(Calculator(n//2, depth))
+    if n-1 > 0:
+        best.append(Calculator(n-1, depth))
+    return min(best)
 
-arr[0] = 0
-arr[1] = 0
-arr[2] = 1
-arr[3] = 1
 
-num = int(sys.stdin.readline())
-for i in range(1, num+1):
-    dp(i)
-print(dp(num))
+N = int(input())
+DP = [None]*(N+1)
+
+for i in range(1, N+1):
+    res = Calculator(i, 0)
+    DP[i] = res
+print(DP[N])
