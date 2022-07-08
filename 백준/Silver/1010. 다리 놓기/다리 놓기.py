dc98101c786dc9fd71bbbd1c3c87f3caaddb2ab8
Sensol2 <80695040+Sensol2@users.noreply.github.com>
2022-07-08 13:40:04 +0900

    [Silver V] Title: 다리 놓기, Time: 112 ms, Memory: 32952 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+import math
+
+
+def combination(n, r):
+    return math.factorial(n) // (math.factorial(r) * math.factorial(n-r))
+
+
+T = int(input())
+
+for i in range(T):
+    N, M = map(int, input().split())
+    print(combination(M, N))
