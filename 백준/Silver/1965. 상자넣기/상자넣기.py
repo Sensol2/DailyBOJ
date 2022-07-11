d9bc41ce934798955d2c31d32e67c81fb42f209f
Sensol2 <80695040+Sensol2@users.noreply.github.com>
2022-07-11 15:15:36 +0900

    [Silver II] Title: 상자넣기, Time: 124 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -0,0 +1,12 @@
+N = int(input())
+arr = []
+
+arr = list(map(int, input().split()))
+DP = [0] * N
+
+for i in range(N):
+    for j in range(i):
+        if arr[j] < arr[i]:
+            DP[i] = max(DP[i], DP[j] + 1)
+
+print(max(DP)+1)
