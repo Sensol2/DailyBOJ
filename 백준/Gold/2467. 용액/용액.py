06f74c709096df977883188d8aa4d1c05d9a1045
Sensol2 <80695040+Sensol2@users.noreply.github.com>
2022-08-15 12:46:46 +0900

    [Gold V] Title: 용액, Time: 160 ms, Memory: 129480 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -2,34 +2,20 @@ import math
 
 N = int(input())
 arr = list(map(int, input().split()))
-base = []  # 알칼리성, -
-acid = []  # 산성, +
-for num in arr:
-    if num < 0:
-        base.append(num)
+
+arr = sorted(arr)
+
+left = 0
+right = len(arr) - 1
+result = [arr[left], arr[right]]
+while left != right:
+    solution = arr[left] + arr[right]
+    if abs(solution) < abs(result[0] + result[1]):
+        result = [arr[left], arr[right]]
+    if solution < 0:
+        left += 1
+    elif solution > 0:
+        right -= 1
     else:
-        acid.append(num)
-
-
-min_value = math.inf
-result = []
-if len(base) >= 2:  # 알칼리 용액만으로 만드는 경우
-    if abs(base[-2] + base[-1]) < min_value:
-        min_value = abs(base[-2] + base[-1])
-        result = [base[-2], base[-1]]
-if len(acid) >= 2:  # 산성 용액만으로 만드는 경우
-    if abs(acid[0] + acid[1]) < min_value:
-        min_value = abs(acid[0] + acid[1])
-        result = [acid[0], acid[1]]
-
-for b in base:
-    prev = math.inf  # 이전 용액
-    for a in acid:
-        if prev < abs(b+a):  # 이전 용액보다 기록이 안좋으면 탈출
-            break
-        if abs(b+a) < min_value:
-            min_value = abs(b+a)
-            result = [b, a]
-        prev = abs(b+a)
-
+        break
 print(' '.join(map(str, result)))
