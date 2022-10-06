2509f3b7ecfb284ef6568c4639de7ad03d47ca3b
Sensol2 <80695040+Sensol2@users.noreply.github.com>
2022-10-07 01:59:29 +0900

    [Silver V] Title: 팩토리얼 분해, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/2057. 팩토리얼 분해/팩토리얼 분해.py
+++ 백준/Silver/2057. 팩토리얼 분해/팩토리얼 분해.py
@@ -6,28 +6,13 @@ def Sol(n):
         print("NO")
         return
 
-    limit = 10000000
-    while True:
-        for i in range(limit):
-            if n < math.factorial(i+1):
-                n -= math.factorial(i)
-                limit = i
-                break
-            if i == limit - 1:
-                n -= math.factorial(i)
-                limit = i
-                break
-
-        if n == 0:
-            print("YES")
-            return
-
-        if limit == 0:
-            if n == 0:
-                print("YES")
-            else:
-                print("NO")
-            return
+    for i in range(20, -1, -1):
+        if n >= math.factorial(i):
+            n -= math.factorial(i)
+    if n == 0:
+        print("YES")
+    else:
+        print("NO")
 
 
 N = int(input())
