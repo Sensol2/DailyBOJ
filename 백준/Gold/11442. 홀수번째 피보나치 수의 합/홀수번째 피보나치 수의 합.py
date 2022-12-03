ba3a5f94e1871e94cddb5d3ab59760bcd2e8b814
Sensol2 <80695040+Sensol2@users.noreply.github.com>
2022-12-03 12:39:40 +0900

    [Gold II] Title: 홀수번째 피보나치 수의 합, Time: 112 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Gold/11442. 홀수번째 피보나치 수의 합/홀수번째 피보나치 수의 합.py
@@ -0,0 +1,41 @@
+def matrix_mult(A, B):
+    matrix = [[0] * len(A) for _ in range(len(B[0]))]
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            for k in range(len(B[0])):
+                matrix[i][k] += A[i][j] * B[j][k]
+
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            matrix[i][j] %= 1000000007
+
+    return matrix
+
+
+def matrix_pow(A, n):
+    if n == 0:
+        return [[0, 0], [0, 0]]
+    if n == 1:  # 지수가 1인 경우
+        for i in range(len(A)):
+            for j in range(len(A[0])):
+                A[i][j] %= 1000000007
+        return A
+
+    if n % 2 == 0:
+        res = matrix_pow(A, n//2)
+        return matrix_mult(res, res)
+    else:
+        res = matrix_pow(A, n-1)
+        return matrix_mult(res, A)
+
+
+N = int(input())
+mat = [[1, 1], [1, 0]]
+sum_v = 0
+
+if N % 2 != 0:  # 홀수
+    resultMatrix = matrix_pow(mat, (N+1)//2*2)
+else:
+    resultMatrix = matrix_pow(mat, (N)//2*2)
+sum_v += resultMatrix[0][1]
+print(sum_v)
