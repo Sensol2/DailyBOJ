fc28bad128582cb0f79c078376b3b15ed8e218f5
Sensol2 <80695040+Sensol2@users.noreply.github.com>
2023-02-14 12:37:09 +0900

    [Gold V] Title: 평범한 배낭, Time: 596 ms, Memory: 202644 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,32 +1,26 @@
-def GrabMaximumValue(itemList, weight):
-    value = [0, ]
-    
-    for idx, item in enumerate(itemList):
-        if item[0] > weight:
-            break
-        else:
-            if idx == 0:
-                value.append(item[1])
-            else:
-                rest_w = weight - item[0]
-                value.append(item[1] + DP[rest_w][idx-1])
+import sys
 
-            # value.append(item[1] + GrabMaximumValue(itemList[:idx], weight - item[0]))
-    return max(value)
-
+input = sys.stdin.readline
 
 N, K = map(int, input().split())
-
-DP = [[0]*N for _ in range(K+1)]
-items = []
+item = [[0, 0]]*(N+1)
 for i in range(N):
     w, v = map(int, input().split())
-    items.append((w, v))
+    item[i] = [w, v]
 
-# 무게에 따라 오름차순 정렬
-items = sorted(items, key=lambda x: x[0])
+item = sorted(item, key=lambda x: x[0])
+DP = [[0] * (N+1) for _ in range(K+1)]
 
-for i in range(K+1):
-    for j in range(N):
-        DP[i][j] = GrabMaximumValue(items[:j+1], i)
-print(max(DP[K]))
+result = 0
+for i in range(K+1):  # 최대 무게
+    for j in range(N+1):  # 아이템 인덱스
+        if i == 0 and j == 0:
+            continue
+        elif item[j][0] > i:  # 용량초과
+            DP[i][j] = DP[i][j-1]
+        else:
+            DP[i][j] = max(item[j][1] + DP[i-item[j][0]][j-1], DP[i][j-1])
+
+        if DP[i][j] > result:
+            result = DP[i][j]
+print(result)
